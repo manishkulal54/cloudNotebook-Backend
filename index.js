9be449ef877af3016a1059353fee9aeb1fddeeaa
Manish <manishkulal54@gmail.com>
2022-09-10 08:06:00 +0530

    commit

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ dotenv.config()
 
 const port=process.env.PORT || 3001
 
-app.use(cors())
-// app.use(
-//     cors({
-//       origin: ["https://upbeat-northcutt-4650a4.netlify.app"],
-//       origin: ["https://manishkulal-icloudnotebook.netlify.app"],
-//       methods: ["GET", "POST", "DELETE"],
-//       credentials: true,
-//       origin: true,
-//     })
-//   );
+// app.use(cors())
+app.use(
+    cors({
+      origin: ["https://upbeat-northcutt-4650a4.netlify.app"],
+      origin: ["https://manishkulal-icloudnotebook.netlify.app"],
+      methods: ["GET", "POST", "DELETE"],
+      credentials: true,
+      origin: true,
+    })
+  );
   
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+//   });
 app.use(express.json())
 
 app.use('/api/auth',require('./routes/User'))
